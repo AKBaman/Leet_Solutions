9877ad5b9c83b3a30965dfaaa7e008d4e409c3c2
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-02-12 20:33:57 +0530

    Time: 7 ms (5.91%), Space: 12.1 MB (8.36%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int res = -1;
-        while((res = s.find(part,res+1)) != string::npos){
-            s.erase(s.begin() + res,s.begin() + res + part.size());
-            res=-1;
+        // int res = -1;
+        // while((res = s.find(part,res+1)) != string::npos){
+        //     s.erase(s.begin() + res,s.begin() + res + part.size());
+        //     res=-1;
+        // }
+        // return s;
+
+        string ans;
+        int m = part.size();
+        for(char c : s){
+            ans.push_back(c);
+
+            if(ans.size() >= m && ans.substr(ans.size() - m) == part){
+                ans.erase(ans.size() - m);
+            }
         }
-        return s;
+
+        return ans;
     }     
 };

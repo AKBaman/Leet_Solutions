3d0b4627b67f5bda61bbf76abff791480babcd6f
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-02-22 12:20:55 +0530

    Time: 3 ms (33.76%), Space: 15.1 MB (26.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    void generateSubs(vector<int> arr,int index,int n,vector<vector<int>>& ans,vector<int> temp){
+    void generateSubs(vector<int> arr,int index,int n,vector<vector<int>>& ans,vector<int>& temp){
         if(index==n){
             ans.push_back(temp);
             return;
         }
+        //Not Included
         generateSubs(arr,index+1,n,ans,temp);
+        //included
         temp.push_back(arr[index]);
         generateSubs(arr,index+1,n,ans,temp);
+        temp.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();

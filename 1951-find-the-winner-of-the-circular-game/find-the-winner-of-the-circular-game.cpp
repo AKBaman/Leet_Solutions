943aae3b59d369d716b855e6cbf287987726bba0
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-03-16 16:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (66.67%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-    int Winner(vector<bool>& person,int n,int  k,int index,int person_left){
-        if(person_left==1){
-            for(int i=0;i<n;i++){
-                if(person[i]==0){
-                    return i+1;
-                }
-            }
+    int Winner(int  n,int k){
+        if(n==1){
+            return 0;
         }
-
-        int kill = (k-1)%person_left;
-
-        while(kill--){
-            index = (index+1)%n;
-            while(person[index]==1){
-                index = (index+1)%n;
-            }
-        }
-        person[index]=1;
-
-        while(person[index]==1){
-            index = (index+1)%n;
-        }
-
-        return Winner(person,n,k,index,person_left-1);
+        return (Winner(n-1,k) + k)%n;
     }
     int findTheWinner(int n, int k) {
-        //Array for keeping the check which person has been killed already;
-        vector<bool>person(n,0);
-        int person_left = n;
-        int index=0;
-        return Winner(person,n,k,index,person_left);
+        
+        return Winner(n,k)+1;
     }
 };

8f0e66ab965552e66a7af6b4043fcd82b84c3c41
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-03-27 15:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (72.47%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,29 +10,34 @@
  * };
  */
 class Solution {
-
-private:
-    int height(TreeNode* root){
-        if(!root){
-            return 0;
-        }
-
-        int left =  height(root->left);
-        int right = height(root->right);
-
-        return max(left,right)+1;
-    }
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
 
-        int op1  = diameterOfBinaryTree(root->left);
-        int op2 = diameterOfBinaryTree(root->right);
-        int op3 = height(root->left) + height(root->right);
+    pair<int,int> diameterFast(TreeNode* root){
+        if(!root){
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        //first --> diameter second--> height
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
 
-        int ans = max(op1,max(op2,op3));
+        //diameter of left subtree
+        int op1 = left.first;
 
+        //diameter of right subtree
+        int op2 = right.first;
+
+        //height of the right left and a root node
+        int op3 = left.second + right.second;
+
+        pair<int,int> ans;
+        ans.first = max(op1,max(op2,op3));
+        //height wala formula;
+        ans.second = max(left.second,right.second)+1;
+        
         return ans;
-
     }
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameterFast(root).first;
+    }   
 };

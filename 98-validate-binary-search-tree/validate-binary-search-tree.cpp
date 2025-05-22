7bb7df1988708f10a0c09ed94d2487dd85914c28
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-05-22 19:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (9.97%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>& ans){
+        TreeNode* curr = root;
+
+        while(curr)
+        {
+                if(!curr->left)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+
+                while(prev->right != NULL && prev->right!=curr){
+                    prev = prev->right;
+                }
+
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr= curr->left;
+                }
+                else{
+                    prev->right= NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }  
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root,ans);
+
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i+1]<=ans[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};

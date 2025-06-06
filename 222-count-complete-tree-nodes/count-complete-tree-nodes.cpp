2adc11e1a2d4ae3cdcbe080dd0a4a481b597ebb1
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-06-06 14:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (94.23%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,17 +11,35 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root,int& count){
-        if(!root){
-            return;
-        }
-        count++;
-        preorder(root->left,count);
-        preorder(root->right,count);
-    }
     int countNodes(TreeNode* root) {
-        int count  = 0;
-        preorder(root,count);
+        if(!root) return 0;
+        int left  = HeightLeft(root);
+        int right = HeightRight(root);
+
+        //left and right equal means tree is complete so directly returning value
+        if(left  == right) return (1<<left) - 1;
+
+        //otherwise counting the nodes in left and right ans root
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+
+    int HeightLeft(TreeNode* node){
+        if(!node) return 0;
+        int count=0;
+        while(node){
+            count++;
+            node = node->left;
+        }
+        return count;
+    }
+
+    int HeightRight(TreeNode* node){
+        if(!node) return 0;
+        int count=0;
+        while(node){
+            count++;
+            node = node->right;
+        }
         return count;
     }
 };

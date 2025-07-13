1e0f2b130dd4bb56a64dd0189621b14225328435
AKBaman <121811727+AKBaman@users.noreply.github.com>
2025-07-14 02:43:38 +0530

    Time: 3 ms (39.06%) | Memory: 17.2 MB (34.22%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int applyOP(int a , int b, char token){
-        if(token== '+') return a+b;
-        else if(token=='-')return a-b;
-        else if(token =='*')return a*b ;
+    int isSign(char ch,int a,int b){
+        if(ch== '+') return a+b;
+        else if(ch =='-') return a-b;
+        else if(ch == '*') return a*b;
         else return a/b;
-        }
+    }
     int evalRPN(vector<string>& tokens) {
-        stack<int>st;
-        for(string token : tokens){
-            if(token =="+" || token=="-" || token=="*" || token == "/"){
-                int b=st.top();
-                st.pop();
-                int a= st.top();
-                st.pop();
+        stack<int> st;
+        
+       for(string token :  tokens){
+        if(token == "+" || token == "-" || token =="*" || token == "/"){
+            int b = st.top();
+            st.pop();
 
-                st.push(applyOP(a,b,token[0]));
-            }
-            else{
-                st.push(stoi(token));
-            }
+            int a = st.top();
+            st.pop();
+
+            st.push(isSign(token[0],a,b));
         }
+        else{
+            st.push(stoi(token));
+        }
+       }
+
         return st.top();
     }
 };

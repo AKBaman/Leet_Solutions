a5d2acb1501c837fe782eee9f9196d6c638643fa
Aman Kumar Batsh <121811727+AKBaman@users.noreply.github.com>
2025-09-06 16:17:43 +0530

    Time: 0 ms (100%), Space: 12.7 MB (94.05%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,59 +1,25 @@
-class Disjoint{
-    public:
-    vector<int> parent,size;
-    Disjoint(int n){
-        parent.resize(n+1);
-        size.resize(n+1,1);
-        for(int i=0;i<=n;i++){
-            parent[i] = i;
-        }
-    }
-
-    int findPar(int node){
-        if(node == parent[node]){
-            return node;
-        }
-        return parent[node] = findPar(parent[node]);
-    }
-
-    void unionBySize(int u,int v){
-        int m = findPar(u);
-        int n = findPar(v);
-
-        if(m == n) return;
-
-        if(size[m]<size[n]){
-            parent[m] = n;
-            size[n] += size[m];
-        }
-        else{
-            parent[n] = m;
-            size[m]+=size[n];
-        }
-    }
- 
-};
-
-
 class Solution {
 public:
+    vector<int> parent;
+
+    int findPar(int node){
+        return parent[node] == node ? node : parent[node] = findPar(parent[node]);
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        Disjoint ds(n);
+        parent.resize(n+1);
+        iota(parent.begin(),parent.end(),0);
 
-        int ansU = -1,ansV =-1;
-        for(vector<int>& edge :edges){
-            int u = edge[0];
-            int v = edge[1];
+        for(vector<int>& e : edges){
+            int u = findPar(e[0]);
+            int v = findPar(e[1]);
+            if(u == v){
+                return e;
+            }
 
-            if(ds.findPar(u) == ds.findPar(v)){
-               return edge;
-            }
-            else{
-                ds.unionBySize(u,v);
-            }
+            parent[u] = v;
         }
 
-        return  {};
+        return {};
     }
 };

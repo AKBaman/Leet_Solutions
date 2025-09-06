53f31df13bf7db6df9a5e6d73952063d3c84a234
Aman Kumar Batsh <121811727+AKBaman@users.noreply.github.com>
2025-09-06 13:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (75.16%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,27 +1,87 @@
-class Solution {
-private:
-    void dfs(int node,vector<vector<int>>& adj,vector<bool>& vis){
-        vis[node]=1;
-
-        for(int j = 0;j<adj.size();j++){
-            if(adj[node][j] == 1 && !vis[j]){
-                dfs(j,adj,vis);
-            }
-        }
+class DisjointSet{
+  //rank and Parent and size vector
+  
+  public:
+  vector<int> rank,parent,size;
+  DisjointSet(int n){
+    rank.resize(n+1,0);
+    size.resize(n+1,1);
+    parent.resize(n+1);
+    for(int i=0;i<=n;i++){
+      //initializing with them
+      parent[i] = i;
     }
+  }
+
+  //log(n)
+  //finding the ultimate parent of the node 
+  int findUPar(int node){
+    if(parent[node] == node){
+      return node;
+    }
+    return parent[node] = findUPar(parent[node]);
+  }
+
+  void unionByRank(int u ,int v){
+    int ultimatep_u = findUPar(u);
+    int ultimatep_v = findUPar(v);
+
+    if(ultimatep_u == ultimatep_v) return;
+
+    if(rank[ultimatep_u] < rank[ultimatep_v]){
+      parent[ultimatep_u] = ultimatep_v;
+    }
+    else if(rank[ultimatep_u] > rank[ultimatep_v]){
+      parent[ultimatep_v] = ultimatep_u;
+    }
+    else{
+      parent[ultimatep_v] = ultimatep_u;
+      rank[ultimatep_u]++;
+    }
+    
+  }
+  //More logical as we are adding the size of the smaller one to the bigger 
+  //makes more sense
+  void unionBySize(int u ,int v){
+    int ultimatep_u = findUPar(u);
+    int ultimatep_v = findUPar(v);
+
+    //if ultimate parentsa are same 
+    if(ultimatep_u == ultimatep_v) return;
+
+    //check sizes of ultimate parents
+   if(size[ultimatep_u] < size[ultimatep_v]){
+    parent[ultimatep_u] = ultimatep_v;
+    size[ultimatep_v] += size[ultimatep_u];
+   }else{
+    parent[ultimatep_v] = ultimatep_u;
+    size[ultimatep_u]+=size[ultimatep_v];
+   }
+    
+  }
+};
+
+///Using Disjoint set;
+class Solution {
 public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
 
-    int findCircleNum(vector<vector<int>>& adj) {
-        int n = adj.size();
-        int count=0;
-        vector<bool>vis(n,0);
-
+        DisjointSet ds(n);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                count++;
-                dfs(i,adj,vis);
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionBySize(i,j);
+                }
             }
         }
-        return count;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i] == i){
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 };
